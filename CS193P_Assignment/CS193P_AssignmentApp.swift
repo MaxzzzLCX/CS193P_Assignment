d0cab2ec28ddfd12bb6eacbca1381ca90c166338
MaxzzzLCX <cl927@cam.ac.uk>
2023-09-19 17:46:23 +0800

    Initial Commit

---
+++ CS193P_Assignment/CS193P_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193P_AssignmentApp.swift
+//  CS193P_Assignment
+//
+//  Created by Max Lyu on 2023/9/19.
+//
+
+import SwiftUI
+
+@main
+struct CS193P_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
